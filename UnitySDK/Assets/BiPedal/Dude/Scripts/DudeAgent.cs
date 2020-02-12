676d3a74c69c8b89abe2ecdd42739bde47075f9e
taesiri <mtaesiri@gmail.com>
2020-02-13 01:07:04 +0330

    Adding Brain!

--- UnitySDK/Assets/BiPedal/Dude/Scripts/DudeAgent.cs
+++ UnitySDK/Assets/BiPedal/Dude/Scripts/DudeAgent.cs
@@ -1,29 +1,24 @@
-﻿// using MLAgents;
+﻿using MLAgents;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class DudeAgent : MonoBehaviour
+public class DudeAgent : Agent
 {
-    public bool doRandomThings;
-    public float forceMultiplier = 25f;
+    public float forceMultiplier = 200f;
     public Transform hip1Transform;
     public Transform hip2Transform;
     public Transform leg1Transform;
     public Transform leg2Transform;
-
     private HingeJoint2D _hip1Joint;
     private HingeJoint2D _hip2Joint;
     private HingeJoint2D _leg1Joint;
     private HingeJoint2D _leg2Joint;
-
     private Rigidbody2D _hip1Rigidbody2D;
     private Rigidbody2D _hip2Rigidbody2D;
     private Rigidbody2D _leg1Rigidbody2D;
     private Rigidbody2D _leg2Rigidbody2D;
-
     public Transform headTransform;
-    public Transform target;
-
+    // public Transform target;
     private Vector3 _hip1StartPosition;
     private Vector3 _hip2StartPosition;
     private Vector3 _leg1StartPosition;
@@ -42,7 +37,7 @@ public class DudeAgent : MonoBehaviour
     private JointMotor2D _motor4;
 
     public Transform floorTransform;
-
+    public DudeHeadScript HeadScript;
     public void Start()
     {
         _hip1Joint = hip1Transform.GetComponent<HingeJoint2D>();
@@ -112,23 +107,31 @@ public class DudeAgent : MonoBehaviour
         leg2Transform.rotation = _leg2StartRotation;
 
         headTransform.rotation = _headInitialRotation;
-
+        HeadScript.HeadBottom = false;
 
         var rx = Random.Range(6, 12);
         var ry = Random.Range(1.5f, 4f);
         if (Random.Range(0, 10) <= 5)
             rx *= -1;
 
-
-
-        target.transform.position = new Vector3(_headInitialPosition.x + rx, _headInitialPosition.y + ry, -2);
-
+        // target.transform.position = new Vector3(_headInitialPosition.x + rx, _headInitialPosition.y + ry, -2);
     }
 
-    // public override void AgentReset()
-    // {
-    //     ManualReset();
-    // }
+    public override void InitializeAgent()
+    {
+        // GeneratedPoints = new Vector3[CircleResolution];
+        // LastVector = Vector2.zero;
+
+        // for (int i = 0; i < CircleResolution; i++)
+        // {
+        //     GeneratedPoints[i] = Vector3.zero;
+        // }
+    }
+
+    public override void AgentReset()
+    {
+        ManualReset();
+    }
 
     private void Update()
     {
@@ -136,179 +139,109 @@ public class DudeAgent : MonoBehaviour
         {
             ManualReset();
         }
-
-        if (doRandomThings)
-        {
-            //    _motor1.motorSpeed = Random.Range(-100f, 100f);
-            //    _hip1Joint.motor = _motor1;
-            //    _motor2.motorSpeed = Random.Range(-100f, 100f);
-            //    _hip2Joint.motor = _motor2;
-
-            //    _motor3.motorSpeed = Random.Range(-100f, 100f);
-            //    _leg1Joint.motor = _motor3;
-            //    _motor4.motorSpeed = Random.Range(-100f, 100f);
-            //    _leg2Joint.motor = _motor4;
-
-
-            // if (Input.GetKeyDown(KeyCode.B))
-            // {
-            //     _motor1.motorSpeed = 50f * Input.GetAxis("Vertical");
-            //     _hip1Joint.motor = _motor1;
-            //     _motor2.motorSpeed = 50f * Input.GetAxis("Horizontal");
-            //     _hip2Joint.motor = _motor2;
-            // }
-            // else
-            // {
-            //     _motor3.motorSpeed = 50f * Input.GetAxis("Vertical");
-            //     _leg1Joint.motor = _motor3;
-            //     Debug.Log(_motor3.motorSpeed);
-            //     _motor4.motorSpeed = 50f * Input.GetAxis("Horizontal");
-            //     _leg2Joint.motor = _motor4;
-            //     Debug.Log(_motor4.motorSpeed);
-            // }
-
-
-             _motor1.motorSpeed = 200f * Input.GetAxis("Vertical");
-            _hip1Joint.motor = _motor1;
-            _motor2.motorSpeed = 200f * Input.GetAxis("Horizontal");
-            _hip2Joint.motor = _motor2;
-
-        }
-
-        //    Debug.Log(CalculateReward());
     }
 
+    public override void CollectObservations()
+    {
+        // 42
 
-    // public override void CollectObservations()
-    // {
-    //     // 42
+        // Target Position Vector3
+        // AddVectorObs(target.position);
 
-    //     // Target Position Vector3
-    //     AddVectorObs(target.position);
+        // Position Vector3
+        AddVectorObs(hip1Transform.position);
+        AddVectorObs(hip2Transform.position);
+        AddVectorObs(leg1Transform.position);
+        AddVectorObs(leg2Transform.position);
+        AddVectorObs(headTransform.position);
 
-    //     // Position Vector3
-    //     AddVectorObs(hip1Transform.position);
-    //     AddVectorObs(hip2Transform.position);
-    //     AddVectorObs(leg1Transform.position);
-    //     AddVectorObs(leg2Transform.position);
-    //     AddVectorObs(headTransform.position);
+        // Rotation Vector4
+        AddVectorObs(hip1Transform.rotation);
+        AddVectorObs(hip2Transform.rotation);
+        AddVectorObs(leg1Transform.rotation);
+        AddVectorObs(leg2Transform.rotation);
+        AddVectorObs(headTransform.rotation);
 
-    //     // Rotation Vector4
-    //     AddVectorObs(hip1Transform.rotation);
-    //     AddVectorObs(hip2Transform.rotation);
-    //     AddVectorObs(leg1Transform.rotation);
-    //     AddVectorObs(leg2Transform.rotation);
-    //     AddVectorObs(headTransform.rotation);
+        // Motor Speed (float)
+        var motor1 = _hip1Joint.motor;
+        var motor2 = _hip2Joint.motor;
+        var motor3 = _leg1Joint.motor;
+        var motor4 = _leg2Joint.motor;
 
-    //     // Motor Speed (float)
-    //     var motor1 = _hip1Joint.motor;
-    //     var motor2 = _hip2Joint.motor;
-    //     var motor3 = _leg1Joint.motor;
-    //     var motor4 = _leg2Joint.motor;
+        AddVectorObs(motor1.motorSpeed);
+        AddVectorObs(motor2.motorSpeed);
+        AddVectorObs(motor3.motorSpeed);
+        AddVectorObs(motor4.motorSpeed);
 
-    //     AddVectorObs(motor1.motorSpeed);
-    //     AddVectorObs(motor2.motorSpeed);
-    //     AddVectorObs(motor3.motorSpeed);
-    //     AddVectorObs(motor4.motorSpeed);
+        // What else?!
+    }
 
-    //     // What else?!
-    // }
+    public override void AgentAction(float[] vectorAction)
+    {
+        _motor1.motorSpeed = vectorAction[0] * forceMultiplier;
+        _hip1Joint.motor = _motor1;
+        _motor2.motorSpeed = vectorAction[1] * forceMultiplier;
+        _hip2Joint.motor = _motor2;
 
-    // public override void AgentAction(float[] vectorAction, string textAction)
-    // {
-    //     if (!doRandomThings)
-    //     {
-    //         // Actions, size = 4
-    //         _motor1.motorSpeed = vectorAction[0] * forceMultiplier;
-    //         _hip1Joint.motor = _motor1;
-    //         _motor2.motorSpeed = vectorAction[1] * forceMultiplier;
-    //         _hip2Joint.motor = _motor2;
+        _motor3.motorSpeed = vectorAction[2] * forceMultiplier;
+        _leg1Joint.motor = _motor3;
+        _motor4.motorSpeed = vectorAction[3] * forceMultiplier;
+        _leg2Joint.motor = _motor4;
 
-    //         _motor3.motorSpeed = vectorAction[2] * forceMultiplier;
-    //         _leg1Joint.motor = _motor3;
-    //         _motor4.motorSpeed = vectorAction[3] * forceMultiplier;
-    //         _leg2Joint.motor = _motor4;
+        if (HeadScript.HeadBottom)
+        {
+            SetReward(-1);
+            Done();
+        }
 
-    //     }
-    //     else
-    //     {
-    //         _motor1.motorSpeed = Random.Range(-100f, 100f);
-    //         _hip1Joint.motor = _motor1;
-    //         _motor2.motorSpeed = Random.Range(-100f, 100f);
-    //         _hip2Joint.motor = _motor2;
+        // Rewards
+        var reward = CalculateReward();
 
-    //         _motor3.motorSpeed = Random.Range(-100f, 100f);
-    //         _leg1Joint.motor = _motor3;
-    //         _motor4.motorSpeed = Random.Range(-100f, 100f);
-    //         _leg2Joint.motor = _motor4;
-    //     }
+        // Reached target
+        if (reward > 10f)
+        {
+            SetReward(reward);
+            Done();
+        }
+        else
+        {
+            SetReward(reward);
+        }
 
-    //     // Rewards
-    //     var reward = CalculateReward();
-
-    //     // Reached target
-    //     if (reward > 1f)
-    //     {
-    //         SetReward(reward);
-    //         Done();
-    //     }
-    //     else
-    //     {
-    //         SetReward(reward);
-    //     }
-
-    //     // Fell off platform
-    //     if (headTransform.position.y < floorTransform.position.y - 1f)
-    //     {
-    //         Done();
-    //     }
-    // }
+        // Fell off platform
+        if (headTransform.position.y < floorTransform.position.y - 1f)
+        {
+            SetReward(-1);
+            Done();
+        }
+    }
 
     private float CalculateReward()
     {
-        var tt = target.position;
-        tt.z = 0;
+        // var tt = target.position;
+        // tt.z = 0;
 
-        var at = headTransform.position;
-        at.z = 0;
+        // var at = headTransform.position;
+        // at.z = 0;
 
-        var dst = Vector2.Distance(tt, at);
+        // var dst = Vector2.Distance(tt, at);
 
-        if (dst < minDistance)
-            return minDistance - dst;
-        return -dst;
+        // if (dst < minDistance)
+        //     return minDistance - dst;
+        // return -dst;
 
+        return (headTransform.position.x - _headInitialPosition.x);
     }
 
-    private void LateUpdate()
+    public override float[] Heuristic()
     {
-        LogPositions();
-    }
+        var action = new float[4];
 
-    private void LogPositions()
-    {
-        var lData = "";
+        action[0] = Random.Range(-1f, 1f);
+        action[1] = Random.Range(-1f, 1f);
+        action[2] = Random.Range(-1f, 1f);
+        action[3] = Random.Range(-1f, 1f);
 
-        lData += _toString(headTransform) + ", " + _toString(hip1Transform)
-                 + ", " + _toString(hip2Transform)
-                 + ", " + _toString(leg1Transform)
-                 + ", " + _toString(leg2Transform);
-
-        // DataLogger.instance.DoLogging(lData);
-    }
-
-    private static string _toString(Vector3 data)
-    {
-        return $"{data.x}, {data.y}";
-    }
-
-    private static string _toString(Quaternion data)
-    {
-        return $"{data.eulerAngles.x}, {data.eulerAngles.y}, {data.eulerAngles.z}";
-    }
-
-    private static string _toString(Transform data)
-    {
-        return _toString(data.position) + ", " + _toString(data.rotation);
+        return action;
     }
 }
---
+++ UnitySDK/Assets/DudeHeadScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeHeadScript : MonoBehaviour
+{
+    public bool HeadBottom = false;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.gameObject.name == "Floor")
+        {
+            HeadBottom = true;
+        }
+    }
+
+}
